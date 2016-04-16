93de053e6052f09898a67c9f2a645a70acbfdb75
Bogdan <bogdan.lebedenko@gmail.com>
2016-04-16 21:31:52 +0300

    [js#4] Play with core js.

---
+++ js/alert.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Bogdan on 4/14/16.
+ * Main js file of this project.
+ * Will work on it all day, all night
+ * to get better results
+ */
+
+'use strict';
+
+//alert('Hello ');
+//alert('world!');
+//alert(1+2+3);
--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,94 @@
 /**
  * Created by Bogdan on 4/14/16.
+ * Main js file of this project.
+ * Will work on it all day, all night
+ * to get better results
  */
 
-alert('Hello world!');
+'use strict';
+
+function addScript(src) {
+    var script = document.createElement('script');
+    script.src = src;
+    script.async = false;
+    document.head.appendChild(script);
+}
+
+addScript('app1.js');
+addScript('app2.js');
+addScript('app3.js');
+
+//alert('Сейчас будет error');
+//[1,3].forEach(alert);
+
+var msg;
+msg = "some text here";
+//alert(msg);
+
+var newMsg = "some other text";
+//alert(newMsg);
+
+var user = "Bogdan", age = 23, gender = "M";
+
+if(gender == "M") {
+    gender = 'Male';
+} else if (gender == "F") {
+    gender = 'Female';
+}
+
+var concat = user + " - " + age + "," + gender;
+//alert(concat);
+
+var $ = 12;
+var _ = 13;
+//alert($ + _);
+
+// Constants
+
+var COLOR_GREEN = "green";
+var COLOR_ORANGE = "orange";
+
+var name, admin;
+
+var name = "Василий";
+
+admin = name;
+
+//alert(admin);
+
+var planetName = 'Земля';
+var userName = 'Peter';
+
+/*
+
+    number
+    string
+    boolean
+    null
+    undefined
+    object
+    typeof
+    NaN
+
+*/
+
+var number = 123.213;
+
+var inf = 1/0;
+console.log(inf);
+
+var nnan = "dsfdf" * 2;
+console.log(typeof(nnan));
+
+var string = "Some text";
+
+console.log(typeof(string));
+
+var boolean = true;
+
+var age = null;
+console.log('age - ' + typeof(age));
+
+var x;
+console.log('x - ' + typeof(x));
 
