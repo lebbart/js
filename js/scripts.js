8d68d3f87db64a89a8f8f012f271f585904a8fbe
Bogdan <bogdan.lebedenko@gmail.com>
2016-04-17 17:10:36 +0300

    [js#5] Play with core js.

--- js/scripts.js
+++ js/scripts.js
@@ -92,3 +92,196 @@ console.log('age - ' + typeof(age));
 var x;
 console.log('x - ' + typeof(x));
 
+var userData = {
+    "name" : "Bogdan",
+    "age" : 23,
+    "sex" : "M",
+    "languages" : [
+        "javascript",
+        "php",
+        "mysql",
+        "AngularJS"
+    ]
+};
+
+console.log(userData.name + ", " + userData.age + ", " + userData.sex + ", " + userData.languages[0]);
+
+var persent = 8 % 3;
+console.log(persent);
+
+//var i = 1;
+//i++;
+//console.log(i);
+//
+//var someI = ++i;
+//console.log(i);
+
+var carUsed = [
+    'Opel Astra',
+    'Toyota Corolla',
+    'Mitsubishi Colt',
+    'Toyota Auris',
+    'Mitsubishi Pajero',
+    'Mercedes Vito',
+    'Toyota Yaris'
+];
+
+for (var i = 0; i <= carUsed.length ;i++) {
+    console.log(carUsed[i]);
+}
+
+var item = carUsed[Math.floor(Math.random()*carUsed.length)];
+console.log(item);
+
+var title = 'Сколько вам лет?';
+var def = 23;
+
+//var userAge = prompt(title, def);
+//if (userAge == null) {
+//    alert('Вы отменили данное окно.');
+//} else {
+//    alert('Ваш возраст - ' + userAge);
+//}
+//
+//var isAdmin = confirm("Is you Admin?");
+//if (isAdmin) {
+//    alert('Yes, I\'am ');
+//} else {
+//    alert('No, I\'am not ');
+//}
+//
+//var someName = prompt('Укажите ваше имя:', '');
+//
+//if(someName){
+//    alert(someName);
+//}
+
+//var specYear = prompt('В каком году появилась спецификация ECMA-262, ...?', '');
+//if(specYear == 2011) {
+//    alert('Это очень точный и правильный ответ!');
+//} else if (specYear > 2011 && specYear <= 2016) {
+//    alert('Это поздновато');
+//} else if (specYear < 2011) {
+//    alert('Это рановато.')
+//}
+
+var newCar = carUsed[3] == "Toyota Auris" ? "Toyota Avalon" : "Toyota Corolla";
+console.log(newCar);
+
+//var ecma = prompt('«Каково «официальное» название JavaScript?»', '');
+//if(ecma == "ECMAScript") {
+//    alert('Все верно!');
+//} else {
+//    alert('Не знаете? «ECMAScript»!»');
+//}
+
+//var newPromt = prompt('Введите число!', '');
+
+//if(newPromt < 0) {
+//    alert(-1);
+//} else if (newPromt > 0) {
+//    alert(1);
+//} else {
+//    alert(0);
+//}
+
+// Administrator pass
+//
+//var authenthification = prompt("Вы админ?", '');
+//
+//if (authenthification == "admin") {
+//    var password = prompt("Введите пароль", '');
+//
+//    if (password == "admin123") {
+//        alert("Добро пожаловать!");
+//    } else if (!password) {
+//        alert('Вход отменен');
+//    } else {
+//        alert('Пароль не принят!');
+//    }
+//
+//} else if(!authenthification) {
+//    alert("Вход отменен!");
+//} else {
+//    alert("Я вас не знаю.");
+//}
+
+//if (a + b < 4) {
+//    var result = 'Мало';
+//} else  {
+//    var result = 'Много';
+//}
+//
+//var result = (a+b < 4) ? 'Мало' : 'Много';
+
+//var message;
+//
+//if (login == 'Вася') {
+//    message = 'Привет';
+//} else if (login == 'Директор') {
+//    message = 'Здравствуйте';
+//} else if (login == '') {
+//    message = 'Нет логина';
+//} else {
+//    message = '';
+//}
+
+
+var login = 'Вася';
+
+var message = (login == 'Вася') ?
+    'Привет' : (login == 'Директор') ?
+    'Здравствуйте' : (login == '') ?
+    'нет логина' :
+    '';
+
+console.log(login + ", " + message);
+
+var workingTime = {
+    "hour" : '12',
+    "weekend" : true
+};
+
+
+if (workingTime.hour < 10 || workingTime.hour > 18 || workingTime.weekend == true) {
+    console.log('Офис закрыт.');
+} else {
+    console.log('На данный момент офис работает!');
+}
+
+var someAge = 1;
+if (someAge >= 18 && someAge <= 90) {
+    console.log('Да, ваш возраст в промежутке от 18 до 90');
+}
+
+if (someAge < 18 || someAge > 90) {
+    console.log('Ваш возраст либо меньше 18 либо больше 90');
+}
+
+var nummber = "123";
+var newNumb = Number(number);
+
+console.log(typeof newNumb);
+
+var k = 0;
+while(k < 3) {
+    k++;
+    console.log('Now k = ' + k);
+}
+
+for (var i = 1; i <= 10; i++) {
+    console.log('For i = ' + i);
+}
+
+var sum = 0;
+while (true) {
+    var value = +prompt("Введите число",'');
+    if(!value) break;
+    sum += value;
+}
+console.log('Сумма = ' + sum);
+
+for (var i = 0; i <= 11; i++) {
+    if (i % 2 == 0) continue;
+    console.log(i);
+}
