361932346f50d9fb9c730912783cfc2f3c832deb
Bogdan <bogdan.lebedenko@gmail.com>
2016-05-05 17:40:38 +0300

    [js#13]  Data structures -> Objects options [done]

--- js/methods/objects-options.js
+++ js/methods/objects-options.js
@@ -1,80 +1,177 @@
 /**
  * Created by Bogdan on 5/4/16.
- * Objects like assosiative array
+ * Objects options
  *
  *
  */
 
 'use strict';
 
-//o = new Object();
-//obj = {};
+//for...in
 
-var person = {};
+//for (key in obj) {
+//    ...
+//}
 
-person.name = 'Богдан';
-person.age = 23;
+//for (var key in obj) ...
 
-console.log(person.name + ' : ' + person.age);
-
-delete person.age;
-
-if ('name' in person) {
-    console.log('True');
-} else {
-    console.log('False');
-}
-
-console.log(person.laaaaa === undefined);
-console.log(person.name === undefined);
-
-var object = {
-    test: undefined,
-    some: 'more',
-    name: 'Bogdan'
+var menu = {
+    width : '200px',
+    height : '150px',
+    title : 'JavaScript'
 };
 
-console.log(person.test === undefined); // true
-console.log(person.someee === undefined); // true
+for (var key in menu) {
+    console.log('Ключ: ' + key + ', значение: ' + menu[key]);
+}
 
-console.log('name' in object); // true
-console.log('name1' in object); // false
+/*
+ *
+ * Количество свойств в объекте
+ *
+ */
 
-var car = {};
-car['brand'] = 'Tesla';
-car['model'] = 'Model X';
+var menuSecond = {
+    width : '200px',
+    height : '150px',
+    title : 'JavaScript'
+};
 
-// Start
-// Доступ через переменную (квадратные скобки)
+var counter = 0;
 
-var brandMore = 'brand';
-console.log(car[brandMore]);
-console.log(car['model']);
+for (var key in menu) {
+    counter++;
+}
 
-// End
+console.log('Counted options: ' + counter);
 
-var menuSetup = {
-    'width' : '200px',
-    'height' : '200px',
-    'title' : 'Menu',
-    'color' : '#000'
+// end
+
+var codes = {
+    '+7' : 'Russian',
+    '+3' : 'Ukraine',
+    '+1' : 'USA'
 };
 
 var user = {
-    name: 'Таня',
-    age: 23,
-    size: {
-        top: 90,
-        middle: 60,
-        bottom: 90
-    }
+    name : 'Andrew',
+    surname : 'Petrov'
 };
 
-console.log(user.name);
-console.log(user.size.top);
+user.age = 35;
 
-var userOne = {};
-userOne.name = 'Вася';
-userOne.surname = 'Петров';
-userOne.name = 'Сергей';
-delete userOne.name;
+for (var key in user) {
+    console.log(key, user[key]);
+}
+
+for (var code in codes) {
+    var value = codes[code];
+    code = +code;
+
+    console.log(code + ': ' + value);
+}
+
+/*
+ *
+ * Homework
+ *
+ */
+
+/*
+ *
+ * Task 1
+ *
+ */
+
+function isEmpty(obj) {
+    for (var key in obj) {
+        return false
+    }
+    return true;
+}
+
+var someObj = {};
+var someOb = {
+  name : 'asrewrg'
+};
+
+console.log(isEmpty(someOb));
+console.log(isEmpty(someObj));
+
+
+/*
+ *
+ * Task 2
+ *
+ */
+
+var salaries = {
+    peter : 1000,
+    alex : 2000,
+    andrew : 1240
+};
+
+var summ = 0;
+
+for (var name in salaries) {
+    summ += salaries[name];
+}
+
+console.log("Сумма з/п - " + summ);
+
+/*
+ *
+ * Task 3
+ *
+ */
+
+var salariesMore = {
+    'peter' : 1000,
+    'alex' : 2000,
+    'andrew' : 1240,
+    'kostya' : 12450
+};
+
+var manWithBig = '',
+    salaryBig = 0;
+for (var more in salariesMore) {
+    if (salaryBig <= salariesMore[more]) {
+        salaryBig = salariesMore[more];
+        manWithBig = more;
+    }
+}
+
+console.log(manWithBig || 'Нет сотрудников');
+
+/*
+ *
+ * Task 4
+ *
+ */
+
+var newObj = {
+    'Tesla' : 10000,
+    'Mercedes' : 15000,
+    'Toyota' : 14155,
+    'Name' : 'Ivan',
+    'Age' : 25,
+    'Language' : 'Russian'
+};
+
+function isNumeric(n) {
+    return !isNaN(parseFloat(n) && isFinite(n));
+}
+
+console.log(newObj);
+
+function multiplyNumeric(obj) {
+    for (var key in obj) {
+        if(isNumeric(obj[key])) {
+            obj[key] *= 2;
+        }
+    }
+
+    return obj;
+}
+
+console.log(multiplyNumeric(newObj));
